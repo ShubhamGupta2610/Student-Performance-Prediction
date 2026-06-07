da421bccf7b3016e8f2fa1a8ad5ea24dd6905216
ShubhamGupta2610 <shubhamgupta262005@gmail.com>
2026-06-07 19:00:20 +0530

    Refactor Flask app to improve predict_datapoint function

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-
-application = Flask(__name__)
-
-app = application
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -16,8 +13,10 @@ def index():
 
 @app.route('/predictdate', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method == 'Get':
+
+    if request.method == 'GET':
         return render_template('home.html')
+
     else:
         data = CustomData(
             gender=request.form.get('gender'),
@@ -27,14 +26,17 @@ def predict_datapoint():
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('reading_score')),
             writing_score=float(request.form.get('writing_score'))
-
         )
+
         pred_df = data.get_data_as_data_frame()
-        print(pred_df)
 
         predict_pipeline = PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
-        return render_template('home.html', results=results[0])
-    
+        results = predict_pipeline.predict(pred_df)
+
+        return render_template(
+            'home.html',
+            results=results[0]
+        )
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
